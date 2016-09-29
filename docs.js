5aa9ce58fa0fabd853d71105223b4f41acf74653
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2016-09-29 18:12:18 -0400

    Have Alexa respond with a hardcoded response.     -Tested Google Doc code, and successfully lists 10 last files     -TO DO:         - See if I can access the user information

--- docs.js
+++ docs.js
@@ -10,21 +10,17 @@ var googleAuth = require('google-auth-library');
 var SCOPES = [ 'https://www.googleapis.com/auth/drive'];
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'drive-nodejs-quikcstart.json';
-var files = new Object();
 
-//Load client secrets from a local file
-module.exports = function loadClientSecrets(clientSecretsFile, callback, accessToken, responseCallBack) {
-	fs.readFile(clientSecretsFile.toString(), function processClientSecrets(err, content) {
-		if(err) {
-			//console.log('Error loading client secret file: ' + err)
-			return;
-		}
-		//Authorize a client with the loaded credentials, then call the 
-		//Drive API
-		authorize(JSON.parse(content), listFiles, accessToken, callback);
-	});
-}
-
+// Load client secrets from a local file.
+fs.readFile('client_secret_installed.json', function processClientSecrets(err, content) {
+  if (err) {
+    console.log('Error loading client secret file: ' + err);
+    return;
+  }
+  // Authorize a client with the loaded credentials, then call the
+  // Drive API.
+  authorize(JSON.parse(content), listFiles);
+});
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -33,37 +29,22 @@ module.exports = function loadClientSecrets(clientSecretsFile, callback, accessT
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
- function authorize(credentials, listFilesFunction, token, callback){
- 	var clientSecret = credentials.web.client_secret;
- 	var clientId = credentials.web.client_id;
- 	var redirectUrl = credentials.web.redirect_uris[4];
+ function authorize(credentials, callback){
+ 	var clientSecret = credentials.installed.client_secret;
+ 	var clientId = credentials.installed.client_id;
+ 	var redirectUrl = credentials.installed.redirect_uris[0];
  	var auth = new googleAuth();
  	var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
- 	//var filesList = new Object();
-
- 	//Verify token exists
- 	if(token == undefined)
- 	{
- 		console.log("token does not exist")
- 		getNewToken(oauth2Client, listFilesFunction, callback);
- 	}
- 	else{
- 		console.log("token exists");
- 		console.log(token);
- 		oauth2Client.credentials = token;
- 		listFilesFunction(oauth2Client, callback);
- 	}
- 	
- 	/*//Check if we have previously stored a token
+ 	//Check if we have previously stored a token
  	fs.readFile(TOKEN_PATH, function(err, token) {
  		if(err) {
  			getNewToken(oauth2Client, callback);
  		}
  		else{
  			oauth2Client.credentials = JSON.parse(token);
- 			listFilesFunction(oauth2Client, callback);
+ 			callback(oauth2Client);
  		}
- 	});*/
+ 	});
  }
 
  /**
@@ -74,7 +55,7 @@ module.exports = function loadClientSecrets(clientSecretsFile, callback, accessT
  * @param {getEventsCallback} callback The callback to call with the authorized
  *     client.
  */
- function getNewToken(oauth2Client, listFilesFunction, callback){
+ function getNewToken(oauth2Client, callback){
  	var authUrl = oauth2Client.generateAuthUrl({
  		access_type: 'offline',
  		scope: SCOPES
@@ -93,7 +74,7 @@ module.exports = function loadClientSecrets(clientSecretsFile, callback, accessT
  			}
  			oauth2Client.credentials = token;
  			storeToken(token);
- 			listFilesFunction(oauth2Client, callback);
+ 			callback(oauth2Client);
  		});
  	});
  }
@@ -120,10 +101,10 @@ module.exports = function loadClientSecrets(clientSecretsFile, callback, accessT
  *
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
- function listFiles(auth, callback) {
+ function listFiles(auth) {
  	var service = google.drive('v3');
  	service.files.list({
- 		auth: auth, 
+ 		auth: auth,
  		pageSize: 10,
  		fields: "nextPageToken, files(id, name)"
  	}, function(err, response){
@@ -137,8 +118,12 @@ module.exports = function loadClientSecrets(clientSecretsFile, callback, accessT
  			console.log(' No files found.');
  			callback(err);
  		} else {
- 			//send files to SPEECH_OUTPUT
- 			callback(null, files);
+ 			console.log('Files:');
+		    for (var i = 0; i < files.length; i++) {
+		      var file = files[i];
+		      console.log('%s (%s)', file.name, file.id);
+		    }
  		}
  	});
  }
+
--- service.js
+++ service.js
@@ -1,35 +1,31 @@
  //enables strict mode which helps catch common JS programming blunders
- 'use strict';
- var APP_ID = 'amzn1.ask.skill.9fadc9b7-4377-44f0-9d40-9e7f015a8bf7';
- var AlexaSkill = require('./AlexaSkill');
- var SPEECH_OUTPUT = new Object();
- const docs = require('./docs.js');
- var clientSecretsFile = 'client_secret.json';
- docs(clientSecretsFile, callback);
-//docs(clientSecretsFile, callback);
+'use strict';
+var APP_ID = 'amzn1.ask.skill.9fadc9b7-4377-44f0-9d40-9e7f015a8bf7';
+var AlexaSkill = require('./AlexaSkill');
+var SPEECH_OUTPUT = 'Hello, here are your list of files good sir';
+const docs = require('./docs.js');
+var clientSecretsFile = 'client_secret.json';
+
 
 //Define a ListFilesService function which inherits from AlexaSkill.js class
- var ListFilesService = function() {
+var ListFilesService = function() {
  	AlexaSkill.call(this, APP_ID);
 };
 
-
- ListFilesService.prototype = Object.create(AlexaSkill.prototype);
+ListFilesService.prototype = Object.create(AlexaSkill.prototype);
 
 //builds a response to Alexa skill interface and 
 //tells Alexa how to respond to users request
  var ListFilesResponseFunction = function(intent, session, response) {
- 		
- 		 	response.tell(SPEECH_OUTPUT);	
- 		//add files that google drive spits out 
-} 
+ 		 	response.tell(SPEECH_OUTPUT);
+ };
 
 //this will be invoked when the user first launches or opens the skill with its invocation name 
 //this is triggered when said "Alexa, list of files"
  ListFilesService.prototype.eventHandlers.onLaunch = ListFilesResponseFunction;
 
  ListFilesService.prototype.intentHandlers = {
- 	'HelloWorldIntent' : ListFilesResponseFunction
+ 	'ListFilesIntent' : ListFilesResponseFunction
  };
 
  exports.handler = function(event, context) {
@@ -49,3 +45,6 @@ function callback(err, files) {
 	}   
 	console.log(SPEECH_OUTPUT);
  };
+
+
+
