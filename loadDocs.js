6f84df97f6bac7609218a5db6899c1cc59a57cf5
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2016-10-12 14:31:17 -0400

    Made style changes to meet with Standard style

--- loadDocs.js
+++ loadDocs.js
@@ -1,18 +1,16 @@
-//docs.js
+// docs.js
 
-var fs = require('fs');
+var fs = require('fs')
 var authorize = require('./google/authorize.js')
 var listFiles = require('./google/listFiles.js')
 
-
-module.exports = function loadDocs(clientSecretsFile, accessToken, SPEECH, fillOutput, sendResponseCB) {
-	fs.readFile(clientSecretsFile.toString(), function processClientSecrets(err, content) {
-		if(err){
-			console.log('Error Loading client secret file: ' + err);
-			return;
-		}
-		authorize(JSON.parse(content), listFiles, accessToken, SPEECH, fillOutput, sendResponseCB);
-	});
+module.exports = function loadDocs (clientSecretsFile, accessToken, SPEECH, fillOutput, sendResponseCB) {
+  fs.readFile(clientSecretsFile.toString(), function processClientSecrets (err, content) {
+    if (err) {
+      console.log('Error Loading client secret file: ' + err)
+      return
+    }
+    authorize(JSON.parse(content), listFiles, accessToken, SPEECH, fillOutput, sendResponseCB)
+  })
 }
 
- 
--- service.js
+++ service.js
@@ -1,51 +1,50 @@
- //enables strict mode which helps catch common JS programming blunders
-'use strict';
-var APP_ID = 'amzn1.ask.skill.9fadc9b7-4377-44f0-9d40-9e7f015a8bf7';
-var AlexaSkill = require('./AlexaSkill');
-var SPEECH_OUTPUT = new Object();
-const loadDocs = require('./loadDocs.js');
-var clientSecretsFile = 'client_secret.json';
+ // enables strict mode which helps catch common JS programming blunders
+'use strict'
+var APP_ID = 'amzn1.ask.skill.9fadc9b7-4377-44f0-9d40-9e7f015a8bf7'
+var AlexaSkill = require('./AlexaSkill')
+var SPEECH_OUTPUT = ' '
+const loadDocs = require('./loadDocs.js')
+var clientSecretsFile = 'client_secret.json'
 
-//Define a ListFilesService function which inherits from AlexaSkill.js class
-var ListFilesService = function() {
- 	AlexaSkill.call(this, APP_ID);
+// Define a ListFilesService function which inherits from AlexaSkill.js class
+var ListFilesService = function () {
+  AlexaSkill.call(this, APP_ID)
+}
+
+ListFilesService.prototype = Object.create(AlexaSkill.prototype)
+
+// builds a response to Alexa skill interface and
+// tells Alexa how to respond to users request
+var ListFilesResponseFunction = function (intent, session, response) {
+  var token = JSON.stringify(session.user.accessToken)
+  loadDocs(clientSecretsFile, token, SPEECH_OUTPUT, callback, function (speech) {
+    response.tell(speech)
+  })
+}
+
+// this will be invoked when the user first launches or opens the skill with its invocation name
+// this is triggered when said "Alexa, list of files"
+ListFilesService.prototype.eventHandlers.onLaunch = ListFilesResponseFunction
+
+ListFilesService.prototype.intentHandlers = {
+  'ListFilesIntent': ListFilesResponseFunction
+}
+
+exports.handler = function (event, context) {
+  var listFilesService = new ListFilesService()
+  listFilesService.execute(event, context)
+}
+
+function callback (err, files, SPEECH, sendSpeech) {
+  SPEECH = 'Here is your list of files: '
+  if (err) {
+    console.log('Error in callback')
+    return err
+  }
+  for (var i = 0; i < files.length; i++) {
+    SPEECH = SPEECH + ' ' + files[i].name
+    JSON.stringify(SPEECH[i])
+  }
+  sendSpeech(SPEECH)
 };
-
-ListFilesService.prototype = Object.create(AlexaSkill.prototype);
-
-//builds a response to Alexa skill interface and 
-//tells Alexa how to respond to users request
- var ListFilesResponseFunction = function(intent, session, response) {
- 	var token  = JSON.stringify(session.user.accessToken);
- 	loadDocs(clientSecretsFile, token, SPEECH_OUTPUT, callback, function(speech) {
-		response.tell(speech);
- 	});
- };
-
-//this will be invoked when the user first launches or opens the skill with its invocation name 
-//this is triggered when said "Alexa, list of files"
- ListFilesService.prototype.eventHandlers.onLaunch = ListFilesResponseFunction;
-
- ListFilesService.prototype.intentHandlers = {
- 	'ListFilesIntent' : ListFilesResponseFunction
- };
-
- exports.handler = function(event, context) {
- 	var listFilesService = new ListFilesService();
- 	listFilesService.execute(event, context);
- }; 
-
-function callback(err, files, SPEECH, sendSpeech) {
-	SPEECH = 'Here is your list of files: ';
-	if(err){
-		console.log("Error in callback")
-		return err;
-	}
-	for (var i = 0; i < files.length; i++) {
-		SPEECH = SPEECH + ' ' + files[i].name;
-		JSON.stringify(SPEECH[i]);
-	}
-	sendSpeech(SPEECH);
- };
-
 
