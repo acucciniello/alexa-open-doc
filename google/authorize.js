be308ba2cee751efd1bbd43c3d1f6f7242b0ae17
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2016-11-18 19:07:58 -0500

    Add speechAssests folder to hold files for handling speech
    Add IntentSchema.json containing the intents. SampleUtterances.txt
    contains the sample utterances to determine the different intents. Add
    slotTypes and values in FILE_NAMES.txt, FILE_TYPES.txt, and INPUT_TEXT.txt

--- google/authorize.js
+++ google/authorize.js
@@ -8,17 +8,16 @@ var GoogleAuth = require('google-auth-library')
 module.exports = function authorize (credentials, token, callback) {
   var clientSecret = credentials.web.client_secret
   var clientId = credentials.web.client_id
-  var redirectUrl = credentials.web.redirect_uris[4]
+  var redirectUrl = credentials.web.redirect_uris[0]
   var auth = new GoogleAuth()
   var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl)
   // Check if we have previously stored a token
   oauth2Client.setCredentials({
-    access_token: token,
-    refresh_token: 'REFRESH TOKEN HERE'
+    access_token: token
   })
   if (token === undefined) {
     var undefinedToken = 'Token is undefined, please link the skill'
-    callback(undefinedToken)
+    return callback(undefinedToken)
   }
-  callback(null, oauth2Client)
+  return callback(null, oauth2Client)
 }
