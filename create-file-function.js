5fd0feae9a1d4c650dad0d0a0ee20df6285e175c
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2016-12-24 14:48:50 -0500

    Add Logging to understand errors

--- create-file-function.js
+++ create-file-function.js
@@ -3,6 +3,15 @@ var authorize = require('./google/authorize.js')
 var clientSecretsFile = 'client_secret.json'
 var createFile = require('./google/create-file.js')
 var setMimeType = require('./google/set-mime-type.js')
+var winston = require('winston')
+require('winston-loggly-bulk')
+
+winston.add(winston.transports.Loggly, {
+  token: '9174ae8d-8205-4eeb-aa4e-96577486d880',
+  subdomain: 'acucciniello',
+  tags: ['Winston-NodeJS'],
+  json: true
+})
 
 module.exports = CreateFileFunction
 
@@ -16,29 +25,33 @@ function CreateFileFunction (intent, session, response) {
   var fileType = intent.slots.fileType.value
   fs.readFile(clientSecretsFile.toString(), function processClientSecrets (err, content) {
     if (err) {
-      console.log('Error Loading client secret file: ' + err)
       var secretsError = 'There was an issue reaching the skill'
+      winston.log('info', 'There was an issue reaching the skill')
       response.tell(secretsError)
       return
     } else {
       authorize(JSON.parse(content), accessToken, function (err, oauthClient) {
         if (err) {
           var noOauth = 'You must have a linked account to use this skill. Please use the alexa app to link your account.'
+          winston.log('info', 'Failed to auth')
           response.tellWithLinkAccount(noOauth)
           return err
         }
         setMimeType(fileType, function (err, mime) {
           if (err) {
+            winston.log('info', 'Failed to get MIME Type of file')
             response.tell(err)
             return err
           }
           createFile(oauthClient, fileToMake, mime, function (err, name) {
             var fileCreated = 'We created a file named: '
             if (err) {
+              winston.log('info', 'Failed to create file')
               response.tell(err)
               return err
             }
             fileCreated = fileCreated + name
+            winston.log('info', 'We created a file')
             response.tell(fileCreated)
             return
           })
